be06f36e0cbc068583f68b85d452089c8c5acf78
GregoryM0 <gregory.murphy1@stu.ocsb.ca>
2024-10-24 18:25:32 -0400

    slope/length

--- main.js
+++ main.js
@@ -15,6 +15,10 @@ document.getElementById("rectangle_prism_volume").addEventListener("click", rect
 document.getElementById("rectangle_prism_area").addEventListener("click", rect_prism_area);
 document.getElementById("sphere_volume").addEventListener("click", sphere_volume);
 document.getElementById("sphere_area").addEventListener("click", sphere_area);
+document.getElementById("slope1").addEventListener("click", slope1);
+document.getElementById("length1").addEventListener("click", length1);
+
+
 
 
 /*** Functions ***/
@@ -63,10 +67,9 @@ function round_user(value) {
 function length(x1, y1, x2, y2) {
     let answer = delta(x2, x1)**2
     let answer1 = delta(y2, y1)**2
-    let x = Math.sqrt(answer)
-    let y = (answer1)
-    let z = x - y
-    return z 
+    let z = answer + answer1 
+    let x = Math.sqrt(z)
+    return x
 }
 
 //volume of a rectangular prism
@@ -106,7 +109,25 @@ function sphere_area() {
     document.getElementById("answer").textContent = volume
 }
 
+//X1, Y1, X2, Y2 ones//
+function slope1() {
+    let x1 = Number(document.getElementById("x1").value);
+    let y1 = Number(document.getElementById("y1").value);
+    let x2 = Number(document.getElementById("x2").value);
+    let y2 = Number(document.getElementById("y2").value);
+    let answer = slope(x1, y1, x2, y2)
+    document.getElementById("answer1").textContent = answer;
+}
+
+
+
 function length1() {
+    let x1 = Number(document.getElementById("x1").value);
+    let y1 = Number(document.getElementById("y1").value);
+    let x2 = Number(document.getElementById("x2").value);
+    let y2 = Number(document.getElementById("y2").value);
+    let answer = length(x1, y1, x2, y2)
+    document.getElementById("answer1").textContent = answer;
 
 }
 
